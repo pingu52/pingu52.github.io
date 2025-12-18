ff76748e574a5f11a6daced10bf8a305d362fd18
pingu52 <gudrbs9852@gmail.com>
2025-12-19 04:19:01 +0900

    feat: new theme fuwari

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,28 @@
 import { defineCollection, z } from "astro:content";
 
-const posts = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    description: z.string().optional(),
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    tags: z.array(z.string()).default([]),
-    categories: z.union([z.string(), z.array(z.string())]).optional(),
-    company: z.string().optional(),
-    thumbnail: z.string().optional(),
-    popular: z.boolean().optional().default(false),
-    draft: z.boolean().optional().default(false),
-  }),
-});
+const postsCollection = defineCollection({
+	schema: z.object({
+		title: z.string(),
+		published: z.date(),
+		updated: z.date().optional(),
+		draft: z.boolean().optional().default(false),
+		description: z.string().optional().default(""),
+		image: z.string().optional().default(""),
+		tags: z.array(z.string()).optional().default([]),
+		category: z.string().optional().nullable().default(""),
+		lang: z.string().optional().default(""),
 
-export const collections = { posts };
+		/* For internal use */
+		prevTitle: z.string().default(""),
+		prevSlug: z.string().default(""),
+		nextTitle: z.string().default(""),
+		nextSlug: z.string().default(""),
+	}),
+});
+const specCollection = defineCollection({
+	schema: z.object({}),
+});
+export const collections = {
+	posts: postsCollection,
+	spec: specCollection,
+};
