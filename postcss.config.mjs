68f70d570d2ca82df0c39439b46ce0dcf9dca39b
pingu52 <gudrbs9852@gmail.com>
2025-12-20 10:33:12 +0900

    fix: fix github action, update dependencies

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,20 @@
-import postcssImport from 'postcss-import';
-import postcssNesting from 'tailwindcss/nesting/index.js';
-import tailwindcss from 'tailwindcss';
+// import postcssImport from 'postcss-import';
+// import postcssNesting from 'tailwindcss/nesting/index.js';
+// import tailwindcss from 'tailwindcss';
+
+// export default {
+//     plugins: {
+//         'postcss-import': postcssImport,          // to combine multiple css files
+//         'tailwindcss/nesting': postcssNesting,
+//         tailwindcss: tailwindcss,
+//     }
+// };
+import postcssImport from "postcss-import";
+import postcssNesting from "postcss-nesting";
 
 export default {
-    plugins: {
-        'postcss-import': postcssImport,          // to combine multiple css files
-        'tailwindcss/nesting': postcssNesting,
-        tailwindcss: tailwindcss,
-    }
+  plugins: {
+    "postcss-import": postcssImport,
+    "postcss-nesting": postcssNesting,
+  },
 };
